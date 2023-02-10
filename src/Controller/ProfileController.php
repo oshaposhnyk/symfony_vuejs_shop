38380a022372aa5ca3b72ef98155d5181c7cb9ef
olexandrshaposhnyk <shaposhnik@softsprint.net>
2023-02-10 09:44:07 +0200

    add profile edit page

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Form\ProfileEditFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,5 +17,24 @@ class ProfileController extends AbstractController
         return $this->render('main/profile/index.html.twig', [
             'controller_name' => 'ProfileController',
         ]);
+    }
+    #[Route('/profile/edit', name: 'app_profile_edit')]
+    public function edit(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(ProfileEditFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_profile_index');
+        }
+
+        return $this->render('main/profile/edit.html.twig', [
+            'profileEditForm' => $form->createView(),
+        ]);
+
     }
 }
