365029153310c7a53f47c34c451caa2e55c35ab5
olexandrshaposhnyk <shaposhnik@softsprint.net>
2023-02-24 22:19:06 +0200

    add main-cart-show component

---
+++ assets/js/section/main/main-cart-show/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="row">
+    <div class="col-lg-12 order-block">
+      <div class="order-conten">
+        <div v-if="showCartContent">
+          <cart-product-list />
+          <cart-total-price />
+          <v-btn color="success" small @click="makeOrder"> Make order</v-btn>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import CartProductList from "./components/CartProductList.vue";
+import CartTotalPrice from "./components/CartTotalPrice.vue";
+
+export default {
+  name: "App",
+  components: {CartTotalPrice, CartProductList},
+  computed: {
+    showCartContent() {
+      return true;
+    }
+  },
+  methods: {
+    makeOrder() {
+      console.log('Make order');
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ assets/js/section/main/main-cart-show/app.js
@@ -0,0 +1,21 @@
+import { createApp } from 'vue';
+import App from "./App.vue";
+import store from './store/store';
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const app = createApp(App);
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
+
+app.use(vuetify);
+app.use(store);
+
+app.mount('#app');
